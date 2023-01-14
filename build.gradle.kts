caba9777f0224e1d70041849283022f69b4f9c50
Evgenij <theeugenet@gmail.com>
2023-01-14 10:14:48 +0700

    Updated dependencies and little cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,9 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
 
     implementation("net.dv8tion:JDA:5.0.0-beta.2")
-    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
-    implementation("com.github.xzxADIxzx.useful-stuffs:server-menus:3261ff23ac")
+    // implementation("com.github.xzxADIxzx.useful-stuffs:server-menus:3261ff23ac")
 
     implementation("org.mongodb:mongodb-driver-reactivestreams:4.8.1")
     implementation(platform("io.projectreactor:reactor-bom:2020.0.24"))
@@ -47,14 +47,8 @@ val relocate = tasks.register<ConfigureShadowRelocation>("relocateShadowJar") {
 }
 
 tasks.shadowJar {
-    // Makes sure the name of the final jar is (plugin-display-name).jar
     archiveFileName.set("VortexPlugin.jar")
-    // Set the classifier to plugin for publication on a maven repository
     archiveClassifier.set("plugin")
-    // Configure the dependencies shading
     dependsOn(relocate)
-    // Reduce shadow jar size by removing unused classes.
-    // Warning, if one of your dependencies use service loaders or reflection, add to the exclude list
-    // such as "minimize { exclude(dependency("some.group:some-dependency:.*")) }"
     minimize()
 }
