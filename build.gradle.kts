bc56d8caa5efebd1da5140a7b544c7998b1a0806
Evgenij <theeugenet@gmail.com>
2023-02-23 21:16:34 +0700

    нахуя все это

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,21 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
-import fr.xpdustry.toxopid.dsl.anukenJitpack
-import fr.xpdustry.toxopid.dsl.mindustryDependencies
-import fr.xpdustry.toxopid.spec.ModMetadata
-import fr.xpdustry.toxopid.spec.ModPlatform.HEADLESS
+plugins {
+    id("java")
+}
 
 group = "org.ru.vortex"
 version = "1.0.1"
 
-plugins {
-    id("java")
-    id("fr.xpdustry.toxopid") version "3.0.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-}
-
-toxopid {
-    compileVersion.set("v141.3")
-    runtimeVersion.set("v141.3")
-    platforms.add(HEADLESS)
-}
-
 repositories {
     mavenCentral()
-    anukenJitpack()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
-    mindustryDependencies()
+    val mindustryVersion = "v142"
+
+    compileOnly("com.github.Anuken.Arc:arc-core:$mindustryVersion")
+    compileOnly("com.github.Anuken.MindustryJitpack:core:$mindustryVersion")
+    compileOnly("com.github.Anuken.MindustryJitpack:server:$mindustryVersion")
 
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("club.minnced:discord-webhooks:0.8.2")
@@ -39,44 +29,11 @@ dependencies {
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 
-    sourceCompatibility = "16"
-    targetCompatibility = "16"
+    sourceCompatibility = "18"
+    targetCompatibility = "18"
 }
 
-val metadata = ModMetadata(
-        name = "vortex-plugin",
-        displayName = "Vortex Plugin",
-        author = "lucin, OSPx, nekonya, SSTentaclesSS",
-        description = """
-        A main plugin for vortex servers
-
-        Check out our repository:
-        + https://github.com/Vortex-coders/VortexPlugin
-
-        Thanks for contributing:
-        + lucin
-        + OSPx
-        + nekonya
-        + SSTentaclesSS
-    """.trimIndent(),
-        version = project.version.toString(),
-        main = "org.ru.vortex.Vortex"
-)
-
-val relocate = tasks.register<ConfigureShadowRelocation>("relocateShadowJar") {
-    target = tasks.shadowJar.get()
-    prefix = project.property("props.root-package").toString() + ".shadow"
-}
-
-tasks.shadowJar {
-    doFirst {
-        val temp = temporaryDir.resolve("mod.json")
-        temp.writeText(metadata.toJson(true))
-        from(temp)
-    }
-
-    archiveFileName.set("VortexPlugin.jar")
-    archiveClassifier.set("plugin")
-    dependsOn(relocate)
-    minimize()
+tasks.jar {
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
